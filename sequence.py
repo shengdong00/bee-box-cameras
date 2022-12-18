4cad3aa747968e6414fe1a213ed67ce44df10955
shengdong00 <dong0023@qq.com>
2022-12-18 19:53:04 +0800

    complete sequence.py

--- sequence.py
+++ sequence.py
@@ -1,13 +1,15 @@
 #coding=utf-8
 import pandas as pd
-import math
 import random
+
+# NOTE: the './camera_sequence.xlsx' will be overwritten everytime you run this script
 
 df = pd.read_excel('./camera_sequence.xlsx', index_col=0)
 
 round_num = 12*2 # 12*days
 
-for rail in range(1,6):
+turn_lists = []
+for rail in range(len(df)):
     turn_list = []
     for r in range(round_num):
         a = []
@@ -16,10 +18,11 @@ for rail in range(1,6):
             if n not in a:
                 a.append(n)
         turn_list += a
-    
-    for cam in df.index:
-        if df.loc[cam, 'rail']==rail:
-            df.loc[cam, 'turn'] = turn_list
+    turn_lists.append(turn_list)
+
+df['turn'] = turn_lists
+
+df.to_excel('./camera_sequence.xlsx')
 
 print(df)
     
